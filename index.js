b27ed692f392fd31361ee9dff53025d166aaeb4e
Kasper Lewau <lewauu@gmail.com>
2016-07-19 20:03:21 +0200

    fix the colors
    oopsie

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const foregroundColor = '#D8DEE9';
-const backgroundColor = '#1B2B34';
+const foregroundColor = '#fff';
+const backgroundColor = '#1b2b34';
 const cursorColor = foregroundColor;
 const borderColor = backgroundColor;
 const colors = [
-  "#1b2b34",
-  "#343d46",
-  "#4f5b66",
-  "#65737e",
-  "#a7adba",
-  "#c0c5ce",
-  "#cdd3de",
-  "#d8dee9",
+  backgroundColor,
   "#ec5f67",
-  "#f99157",
-  "#fac863",
   "#99c794",
-  "#5fb3b3",
+  "#fac863",
   "#6699cc",
   "#c594c5",
-  "#ab7967"
+  "#5fb3b3",
+  foregroundColor,
+  "#65737e",
+  "#ec5f67",
+  "#99c794",
+  "#fac863",
+  "#6699cc",
+  "#c594c5",
+  "#5fb3b3",
+  foregroundColor
 ];
 
 exports.decorateConfig = config => Object.assign(config, {
